4afbce3888613adba1e25648da195e429a9fe934
Max Schmeling <max@maxschmeling.com>
2024-02-03 01:28:22 -0600

    Add hero section

--- src/app/components/Home/Hero.tsx
+++ src/app/components/Home/Hero.tsx
@@ -1,3 +1,23 @@
 export default function Hero() {
-  return <></>;
+  return (
+    <section>
+      <div className="container flex flex-row my-8 bg-accent">
+        <img src="https://place-hold.it/750x450" alt="hero" />
+        <div className="flex flex-col justify-between p-8 text-center">
+          <h1 className="text-xxxxl font-bold uppercase">
+            Buy Flowers. Fight Cancer.
+          </h1>
+          <p>
+            Every time you buy flowers through our site, we donate our full
+            commission to fund research working to find a cure for TNBC.
+          </p>
+          <div>
+            <a href="https://flowers.com" className="bg-primary text-white p-4">
+              Buy Flowers
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
