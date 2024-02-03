dbdcc233f5ec3ff44a2a025dd15197a2e0e6a027
Max Schmeling <max@maxschmeling.com>
2024-02-03 13:07:18 -0600

    More mobile layout improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,70 +1,91 @@
+"use client";
+
 import Image from "next/image";
 import EdensStory from "./components/Home/EdensStory";
 import Hero from "./components/Home/Hero";
 import Gallery from "./components/Home/Gallery";
 import WhatIsTnbc from "./components/Home/WhatIsTnbc";
 import CallToAction from "./components/Home/CallToAction";
+import { useState } from "react";
 
 export default function Home() {
+  const [navHiddenOnMobile, setNavHiddenOnMobile] = useState(true);
+
   return (
     <>
-      <header className="container flex flex-col items-center gap-8">
-        <nav>
-          <div className="h-20 flex flex-row items-center mx-4 gap-8 mt-0 pt-0">
-            <input className="hidden" type="checkbox" name="" id="" />
-            <div className="block z-10 flex flex-col justify-between gap-1 w-8 h-6 flex-shrink-0">
-              <span className="block h-1 w-full rounded-sm bg-black"></span>
-              <span className="block h-1 w-full rounded-sm bg-black"></span>
-              <span className="block h-1 w-full rounded-sm bg-black"></span>
-            </div>
-            <div className="logo">
-              <Image
-                width={760}
-                height={113}
-                src="/logo.png"
-                alt="Eden's Flower Garden Logo"
-              />
-            </div>
-            <ul className="list-none hidden">
+      <header className="container flex flex-col items-center">
+        <div className="flex flex-row items-center mx-4 gap-8 mt-0 pt-0">
+          <input className="hidden" type="checkbox" name="" id="" />
+          <div
+            className="block md:hidden z-10 flex flex-col justify-between gap-1 w-8 h-6 flex-shrink-0"
+            onClick={() => setNavHiddenOnMobile(!navHiddenOnMobile)}
+          >
+            <span className="block h-1 w-full rounded-sm bg-black"></span>
+            <span className="block h-1 w-full rounded-sm bg-black"></span>
+            <span className="block h-1 w-full rounded-sm bg-black"></span>
+          </div>
+          <div className="md:pt-10 box-border">
+            <Image
+              width={760}
+              height={113}
+              src="/logo.png"
+              alt="Eden's Flower Garden Logo"
+            />
+          </div>
+        </div>
+        <div className="box-border">
+          <nav
+            className={` ${
+              navHiddenOnMobile ? "hidden" : ""
+            } md:block my-4 w-full`}
+          >
+            <ul className="flex flex-col md:flex-row gap-2 space-between uppercase md:justify-center p-8">
               <li>
-                <a href="#">Home</a>
+                <a className="underline" href="#top">
+                  Home
+                </a>
               </li>
               <li>
-                <a href="#">Eden's Story</a>
+                <a className="underline" href="#eden">
+                  Eden’s Story
+                </a>
               </li>
               <li>
-                <a href="#">Gallery</a>
+                <a className="underline" href="#gallery">
+                  Gallery
+                </a>
               </li>
               <li>
-                <a href="#">What is TNBC?</a>
+                <a className="underline" href="#tnbc">
+                  What is TNBC?
+                </a>
               </li>
               <li>
-                <a href="#">Donate</a>
+                <a className="underline" href="#donate">
+                  Donate
+                </a>
               </li>
               <li>
-                <a href="#">Contact</a>
+                <a className="underline" href="#contact">
+                  Contact
+                </a>
               </li>
             </ul>
-          </div>
-        </nav>
-        {/* 
-        <nav className="flex flex-row space-between space-x-4 uppercase">
-          <a href="#top">Home</a>
-          <a href="#eden">Eden’s Story</a>
-          <a href="#gallery">Gallery</a>
-          <a href="#tnbc">What is TNBC?</a>
-          <a href="#donate">Donate</a>
-          <a href="#contact">Contact</a>
-        </nav> */}
+          </nav>
+        </div>
       </header>
-      <main className="flex min-h-screen flex-col justify-between">
+      <main
+        className={`flex min-h-screen flex-col justify-between ${
+          !navHiddenOnMobile ? "opacity-20" : ""
+        } md:opacity-100`}
+      >
         <Hero />
         <EdensStory />
         <Gallery />
         <WhatIsTnbc />
         <CallToAction />
       </main>
-      <footer className="text-center">
+      <footer className="text-center pb-10">
         <p>
           &copy; 2024 Eden's Foundation. All rights reserved. Eden's Foundation
           is a 501(c)(3) tax-exempt organization. ID: 13-1788491.
