f961b8ead4a59d678000f4df6dc5ee12ee6352e3
Brian Alvey <github@subs.frankensite.com>
2024-02-19 16:44:44 -0800

    matching title style for TNBC

--- src/app/components/Home/WhatIsTnbc.tsx
+++ src/app/components/Home/WhatIsTnbc.tsx
@@ -1,9 +1,9 @@
 export default function WhatIsTnbc() {
   return (
-    <section className="bg-primary pb-6 pt-8 text-white" id="tnbc">
+    <section className=" bg-primary pb-6 pt-8 text-white" id="tnbc">
       <div className="content text-center">
         <div className="mx-4 md:mx-12 lg:mx-24">
-          <h2 className="caps">What is TNBC?</h2>
+        <h2 className="text-4xl font-bold mb-4">What is TNBC?</h2>
           <p className="text-xl mb-4">Triple negative breast cancer is a rare and aggressive form of breast
           cancer in which all three receptor tests come back negative. TNBC differs from other types of
           invasive breast cancer in that it tends to grow and spread faster, has fewer treatment options,
